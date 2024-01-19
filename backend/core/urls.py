22f7d5a3fe10673aa76a8ae2a4698407d5e8c307
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-19 15:31:31 +0300

    added view to retrieve previous conversations saved

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from messaging.api_view import MessageViewSet
+from messaging.api_view import MessageViewSet, MessageRetrievePrevious
 
 router = SimpleRouter()
 router.register("message", MessageViewSet)
+router.register("prev/message", MessageRetrievePrevious, basename="messaging")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/messaging/api_view.py
+++ backend/messaging/api_view.py
@@ -1,13 +1,23 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .serializers import MessageSerializer, Message
 from rest_framework.response import Response
 
 
 class MessageViewSet(ModelViewSet):
     serializer_class = MessageSerializer
-    fields = "__all__"
-    queryset = Message.objects.all()
+    queryset = Message.objects.order_by("-id")
 
     def create(self, request):
-        Message.objects.create(**request.data)
+        m=Message.objects.create(**request.data)
+        print(m, m.message, m.message_by, m.group_name)
         return Response({"message": "created"})
+
+
+class MessageRetrievePrevious(ReadOnlyModelViewSet):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        queryset = Message.objects.filter(
+            group_name=self.request.GET['group_name']).order_by("id")
+        print(queryset)
+        return MessageSerializer(queryset, many=True).data
