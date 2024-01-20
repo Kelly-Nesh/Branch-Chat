db0009da6d0edb947b1bb5de2d5350fafc06c807
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-20 13:05:40 +0300

    redirect from ended conversation, chat formatting

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -16,7 +16,7 @@ import { timestamp } from "../App";
     Connects, receives and sends data from the backend
       websocket
   */
-const c = console.log;
+const cl = console.log;
 /* get previous messages from db
  */
 function getMessages(url, setMessagelog, setAgent) {
@@ -24,10 +24,6 @@ function getMessages(url, setMessagelog, setAgent) {
     .get(url)
     .then((e) => {
       setMessagelog(e.data);
-      const hasAgent = e.data.find((e) => {
-        return e.hasAgent;
-      });
-      hasAgent.hasAgent ? setAgent(true) : "";
     })
     .catch((e) => {
       console.log(e.status);
@@ -38,16 +34,24 @@ const Chat = ({ caller }) => {
   const { convo_id } = useParams();
   let ws_url = "ws://localhost:8000/ws/support/" + convo_id + "/";
   const ref = useRef();
+  const lastmsgref = useRef();
   const [messagelog, setMessagelog] = useState([]);
   const navigate = useNavigate();
   const [show, setShow] = useState(true);
   const [alert, setAlert] = useState(false);
   const [sender, setSender] = useState("user");
   const [agent, setAgent] = useState(false);
+  const [end, setEnd] = useState(false);
   const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
     ws_url,
     {
-      shouldReconnect: () => true,
+      onOpen: () => {
+        setShow(true);
+      },
+      shouldReconnect: () => {
+        setShow(true);
+        return true;
+      },
       reconnectAttempts: 5,
     }
   );
@@ -55,13 +59,18 @@ const Chat = ({ caller }) => {
   const backend = "http://localhost:8000/api/";
 
   useEffect(() => {
+    if (lastmsgref.current) {
+      lastmsgref.current.scrollIntoView();
+    }
     if (lastJsonMessage !== null) {
       setMessagelog((prev) => prev.concat(lastJsonMessage));
+      setEnd(lastJsonMessage.complete);
+      setAgent(lastJsonMessage.hasAgent);
+      setShow(false);
     } else {
       /* if new browser agent get chatlog from database */
       const url = `${backend}conv/history/?convo_id=${convo_id}`;
       getMessages(url, setMessagelog, setAgent);
-      // console.log(messagelog);
 
       setShow(false);
     }
@@ -77,23 +86,36 @@ const Chat = ({ caller }) => {
       caller === "user"
         ? localStorage.getItem("customer_id")
         : localStorage.getItem("emp_id");
-    // console.log(sender);
     setSender(sender);
+    const agentissender = !sender.startsWith("user");
     const data = {
       conversation_id: convo_id,
       topic: topic,
       timestamp: timestamp(),
       message: ref.current.value,
       sender: sender,
+      complete: end,
+      hasAgent: agentissender || agent,
     };
-    agent ? (data.hasAgent = true) : null;
     axios.post(backend + "message/", data).catch((e) => {
       console.log(e);
     });
     ref.current.value = "";
     sendJsonMessage(data);
   });
-
+  const handleEnd = () => {
+    const finalmessage = messagelog[messagelog.length - 1].conversation_id;
+    axios
+      .patch(backend + `message/${finalmessage}/`)
+      .then((res) => {
+        setEnd(res.data.complete);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    sendJsonMessage({ complete: true });
+    navigate(-1);
+  };
   let connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting...",
     [ReadyState.OPEN]: "Connected.",
@@ -109,48 +131,86 @@ const Chat = ({ caller }) => {
       style={{ maxWidth: "80%" }}
       className="mx-auto place-items-center"
     >
-      {connectionStatus && show && <Alert>{connectionStatus}</Alert>}
-      <Row className="justify-content-center">
-        <Col md={6} className="overflow-scroll" style={{ height: "75svh" }}>
-          {lastJsonMessage ? <p>{lastJsonMessage.data}</p> : null}
-          <Chatformat messages={messagelog} sender={sender} />
+      {connectionStatus && show && (
+        <Alert className="alert">{connectionStatus}</Alert>
+      )}
+      {end && (
+        <Alert className="alert" variant="danger">
+          Conversation has ended
+        </Alert>
+      )}
+      {alert && (
+        <Alert variant="warning" className="alert">
+          Write a message to send
+        </Alert>
+      )}
+      <Row
+        className="justify-content-center overflow-auto"
+        style={{ height: "70svh" }}
+      >
+        <Col md={6} className="d-flex flex-column mb-2">
+          {/* {lastJsonMessage ? <p>{lastJsonMessage.data}</p> : null} */}
+          <Chatformat
+            messages={messagelog}
+            caller={caller}
+            lastmsgref={lastmsgref}
+          />
         </Col>
       </Row>
       <br />
-      <Row className="justify-content-center">
-        <Col md={6} className="text-center">
-          <Form.Control
-            as="textarea"
-            placeholder="write a message"
-            ref={ref}
-            className="d-block mx-auto mb-2"
-          />
-          <Button
-            variant="primary"
-            disabled={readyState !== ReadyState.OPEN}
-            onClick={handleSend}
-          >
-            Send
-          </Button>
-        </Col>
-      </Row>
+      {end === false && (
+        <Row
+          className="justify-content-center position-relative"
+          style={{ bottom: 0 }}
+        >
+          <Col md={6} className="text-center">
+            <Form.Control
+              as="textarea"
+              placeholder="write a message"
+              ref={ref}
+              className="d-block mx-auto mb-2"
+            />
+            <div className="d-flex justify-content-evenly">
+              {sender.startsWith("user") === false && (
+                <Button
+                  variant="primary"
+                  disabled={readyState !== ReadyState.OPEN}
+                  onClick={handleEnd}
+                >
+                  End Chat
+                </Button>
+              )}
+              <Button
+                variant="primary"
+                disabled={readyState !== ReadyState.OPEN}
+                onClick={handleSend}
+              >
+                Send
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
 
 export default Chat;
-function Chatformat({ messages, sender }) {
+function Chatformat({ messages, caller, lastmsgref }) {
   let align;
-  // console.log(messages)
-  return messages.map((m, idx) => {
-    const sender = m.sender;
-    // console.log(m, m.sender, sender);
-
-    align = sender === m.sender ? "end" : "start";
+  const mapped = messages.map((m, idx) => {
+    const sd = m.sender ? m.sender : "";
+    align =
+      (caller === "user" && sd.startsWith("user")) ||
+      (caller === "agent" && sd.startsWith("user") === false)
+        ? "end"
+        : "start";
     return (
       <p className={`text-${align}`} key={idx}>
         {m.message}
       </p>
     );
   });
+  mapped.push(<p id="last" key={"last"} ref={lastmsgref}></p>);
+  return mapped;
 }
