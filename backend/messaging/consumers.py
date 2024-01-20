b46b12ff90252c206add918adc51321fd56132a0
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-20 13:04:51 +0300

    added conditional for completed chats

--- backend/messaging/consumers.py
+++ backend/messaging/consumers.py
@@ -2,11 +2,15 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync as atos
+from .models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.group_name = self.scope["url_route"]["kwargs"]["group_name"]
+        if Message.objects.filter(conversation_id=self.group_name)\
+            .order_by("-id").first().complete:
+            return
         atos(self.channel_layer.group_add)(self.group_name, self.channel_name)
         self.accept()
 
@@ -19,10 +23,12 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         """Receives"""
         text_json = json.loads(text_data)
-        # print(type(text_data), type(text_json))
         text_json["type"] = "group_message"
-
+        # print(text_json['complete'])
         atos(self.channel_layer.group_send)(self.group_name, text_json)
+        if text_json.get('complete'):
+            self.disconnect(None)
 
     def group_message(self, event):
+        # print(event)
         self.send(text_data=json.dumps(event))
