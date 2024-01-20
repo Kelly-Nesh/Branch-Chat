f80f2f501d5850ba2433ae12358c339084d1b724
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-20 03:31:59 +0300

    removed redundant path

--- backend/messaging/routing.py
+++ backend/messaging/routing.py
@@ -4,6 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     path("ws/support/<str:group_name>/", consumers.ChatConsumer.as_asgi()),
-    path("ws/agent/support/<str:group_name>/",
-            consumers.ChatConsumer.as_asgi()),
+    # path("ws/agent/support/<str:group_name>/",
+    #         consumers.ChatConsumer.as_asgi()),
 ]
