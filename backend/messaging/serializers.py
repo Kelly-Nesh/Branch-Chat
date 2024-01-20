1001e7ec9481a4a4963a2adedde792057098ccb3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-20 03:32:40 +0300

    serializers for new models

--- backend/messaging/serializers.py
+++ backend/messaging/serializers.py
@@ -1,7 +1,20 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Message
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from .models import Message, Customer, Agent
+
 
 class MessageSerializer(ModelSerializer):
     class Meta:
         model = Message
+        fields = "__all__"
+
+
+class CustomerSerializer(ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = "__all__"
+
+
+class AgentSerializer(ModelSerializer):
+    class Meta:
+        model = Agent
         fields = "__all__"
