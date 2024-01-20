0a532a9e111ef730d6d1a3b94ee50865b7c7205a
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-20 03:31:42 +0300

    added Customer and Agent models

--- backend/messaging/models.py
+++ backend/messaging/models.py
@@ -1,13 +1,33 @@
 from django.db import models
-
+from shortuuid import uuid
 
 
 class Message(models.Model):
-    user_id = models.IntegerField()
+    conversation_id = models.CharField(max_length=100)
     timestamp = models.CharField(max_length=100)
     topic = models.CharField(max_length=100)
     message = models.TextField()
-    message_by = models.CharField(max_length=100)
-    group_name = models.CharField(max_length=100)
+    sender = models.CharField(max_length=100)
+    hasAgent = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
-    agent = models.CharField(max_length=100, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.conversation_id:
+            self.conversation_id = uuid(name=self.timestamp)[:8]
+        super().save(*args, **kwargs)
+
+
+class Customer(models.Model):
+    user_id = models.CharField(max_length=100, primary_key=True)
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            self.user_id = 'user_{}'.format(uuid(name=self.username)[:8])
+        super().save(*args, **kwargs)
+
+
+class Agent(models.Model):
+    employee_id = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=100, null=False)
